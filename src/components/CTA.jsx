d7aaa129fd4bf56a7226ee4f021992a0bea8e24c
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-06-11 02:26:24 +0600

    linked with stylesheet

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import Container from '../components/Container';
 import PhoneFrame from './PhoneFrame';
+import styles from './CTA.module.scss';
 
 const CTA = () => (
-  <div>
-    <h3 style={{ textAlign: 'center' }}>
-      Play epic talk shows anywhere, anytime.
-    </h3>
-    <PhoneFrame />
+  <div className={styles.ctaSection}>
+    <Container>
+      <h3>Play epic talk shows anywhere, anytime.</h3>
+      <PhoneFrame />
+      <p>
+        Player FM is the multi-platform podcast app that helps you find shows on
+        the topics you care about and play them at your convenience, even when
+        you&#39;re offline.
+      </p>
+      <a
+        href="https://play.google.com/store/apps/details?id=fm.player"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src="imgs/play-store.png" alt="Get the app from Play Store" />
+      </a>
+    </Container>
   </div>
 );
 
