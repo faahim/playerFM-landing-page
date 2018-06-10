6493b4a39f67f197ebf71d0abba9a50ef321acbf
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-06-10 17:40:39 +0600

    added SVG and Tagline component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,14 @@
-import { Link } from 'gatsby';
 import React from 'react';
 import Container from '../components/Container';
 import Layout from '../components/Layout';
+import WalkSVG from '../components/WalkSVG';
+import Taglines from '../components/Taglines';
 
 const IndexPage = () => (
   <Layout>
     <Container>
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <Link to="/page-2/">Go to page 2</Link>
+      <WalkSVG />
+      <Taglines />
     </Container>
   </Layout>
 );
