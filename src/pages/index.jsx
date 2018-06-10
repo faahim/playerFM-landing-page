13d81ffbe4b0e32543717f4367eb7fb411aa831e
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-06-10 23:30:30 +0600

    added CTA component in main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,14 @@ import Container from '../components/Container';
 import Layout from '../components/Layout';
 import WalkSVG from '../components/WalkSVG';
 import Taglines from '../components/Taglines';
+import CTA from '../components/CTA';
 
 const IndexPage = () => (
   <Layout>
     <Container>
       <WalkSVG />
       <Taglines />
+      <CTA />
     </Container>
   </Layout>
 );
