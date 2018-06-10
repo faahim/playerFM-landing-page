d59b5c0578f331e8762c26d264ba646b6f663d1b
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-06-10 23:29:12 +0600

    added CTA component

---
+++ src/components/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PhoneFrame from './PhoneFrame';
+
+const CTA = () => (
+  <div>
+    <h3 style={{ textAlign: 'center' }}>
+      Play epic talk shows anywhere, anytime.
+    </h3>
+    <PhoneFrame />
+  </div>
+);
+
+export default CTA;
