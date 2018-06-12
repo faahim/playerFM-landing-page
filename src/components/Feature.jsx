85185f4a28043c865a7a6770da726b0ef7dbaa01
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-06-13 05:12:10 +0600

    fixed linting issues

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Container from './Container';
-import styles from './Feature.module.scss';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Container from './Container'
+import styles from './Feature.module.scss'
 
 const Feature = ({ featureSVG, featureTtile, featureDetails }) => (
   <div className={styles.featureItem}>
@@ -20,12 +20,12 @@ const Feature = ({ featureSVG, featureTtile, featureDetails }) => (
       </div>
     </Container>
   </div>
-);
+)
 
 Feature.propTypes = {
   featureTtile: PropTypes.string.isRequired,
   featureDetails: PropTypes.string.isRequired,
   featureSVG: PropTypes.element.isRequired,
-};
+}
 
-export default Feature;
+export default Feature
