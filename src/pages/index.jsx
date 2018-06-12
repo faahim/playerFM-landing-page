70251b670ca72e66132d522ffd84c629e1cfa245
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-06-13 05:14:25 +0600

    added testimonial section to the main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import Layout from '../components/Layout';
-import Banner from '../components/Banner';
-import CTA from '../components/CTA';
-import FeatureSection from '../components/FeatureSection';
+import React from 'react'
+import Layout from '../components/Layout'
+import Banner from '../components/Banner'
+import CTA from '../components/CTA'
+import FeatureSection from '../components/FeatureSection'
+import TestimonialSection from '../components/TestimonialSection'
 
 const IndexPage = () => (
   <Layout>
     <Banner />
     <CTA />
     <FeatureSection />
+    <TestimonialSection />
   </Layout>
-);
+)
 
-export default IndexPage;
+export default IndexPage
