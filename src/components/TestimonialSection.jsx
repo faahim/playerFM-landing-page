51ae1535b56a9dea8cc553b42187cb338c775fe6
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-06-14 09:34:54 +0600

    removed state and logic from the component

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Testimonials from './Testimonials'
 import styles from './TestimonialSection.module.scss'
 
@@ -39,68 +39,24 @@ const testimonialsData = [
     message: 'My life, My life, would suck, would suck, without you!',
     vendor: 'Lifehacker',
   },
+  {
+    message: "Face tomorrow, Tomorrow isn't yesterday.",
+    vendor: 'TechCrunch',
+  },
+  {
+    message: "It's raining again, tell me something I don't know.",
+    vendor: 'The Verge',
+  },
+  {
+    message: 'My life, My life, would suck, would suck, without you!',
+    vendor: 'Lifehacker',
+  },
 ]
 
-class TestimonialSection extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      activeItem: testimonialsData.length - 1,
-    }
-
-    this.nextCard = this.nextCard.bind(this)
-    this.previousCard = this.previousCard.bind(this)
-  }
-
-  nextCard() {
-    this.setState(prevState => ({
-      activeItem: !(prevState.activeItem - 1 < 0)
-        ? prevState.activeItem - 1
-        : 0,
-    }))
-  }
-
-  previousCard() {
-    this.setState(prevState => ({
-      activeItem: !(prevState.activeItem + 1 > testimonialsData.length - 1)
-        ? prevState.activeItem + 1
-        : testimonialsData.length - 1,
-    }))
-  }
-
-  render() {
-    return (
-      <section id={styles.testimonials}>
-        <div
-          style={{
-            transform: `translateX(${(testimonialsData.length - 1) *
-              0}px) translateY(${this.state.activeItem * 15}px) scale(${1 +
-              (testimonialsData.length - 1 - this.state.activeItem) * 0.08}`,
-          }}
-          className={styles.testimonialWrapper}
-        >
-          {testimonialsData.map((testimonial, index) => (
-            <Testimonials
-              message={testimonial.message}
-              vendor={testimonial.vendor}
-              itemNumber={index}
-              activeItem={this.state.activeItem}
-              itemAmount={testimonialsData.length}
-            />
-          ))}
-        </div>
-        <div className={styles.btnContainer}>
-          <button type="button" onClick={this.previousCard}>
-            ◀
-          </button>
-          <button type="button" onClick={this.nextCard}>
-            ▶
-          </button>
-        </div>
-      </section>
-    )
-  }
-}
+const TestimonialSection = () => (
+  <section id={styles.testimonials}>
+    <Testimonials testimonials={testimonialsData} />
+  </section>
+)
 
 export default TestimonialSection
